e4538eae581bd6c194c9d4d6285a9b58dc8eb41a
Darren Kessner <Darren.Kessner@marlborough.org>
2020-10-11 08:30:23 -0700

    first commit

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  // put setup code here
+  createCanvas(400, 400);
+}
+
+let x = 50;
+
+function draw() {
+  // put drawing code here
+  background(0);
+  ellipse(x, 200, 100, 50);
+
+  x++;
+  if (x > width+50) x = -50;
+}
